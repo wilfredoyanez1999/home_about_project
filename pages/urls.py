79555694416f7db988284aac357a74a4bc395024
Wilfredo Yanez <wilfredo123alejandro17@gmail.com>
2025-08-30 17:26:27 -0400

    about page add

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutpageView
 
 urlpatterns = [
     path('josefino/', HomePageView.as_view(), name='home'),
+    path('about/', AboutpageView.as_view(), name='about'),
 ]
     
